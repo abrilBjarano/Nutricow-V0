1a4d3cd18ff4e3f4ef46fca944618238f09b20c8
abrilxcx <abrlbjrn@gmail.com>
2024-01-15 02:22:14 -0600

    header updated

--- src/app/nutricow/components/header/header.component.ts
+++ src/app/nutricow/components/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   template: `
-    <h4 class="text-center text-white">nutricow 🐮</h4>
+    <h4 class="text-center text-white">nutricow 🐄</h4>
   `
 })
 export class HeaderComponent {
